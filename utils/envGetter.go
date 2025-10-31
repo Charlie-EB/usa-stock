f6a33f76eb8e5b9465608f471e254342c7666ebf
Charlie Morris <cshotam@gmail.com>
2025-10-31 12:56:33 +1100

    read .env

--- utils/envGetter.go
+++ utils/envGetter.go
@@ -1,15 +1,42 @@
 package utils
 
-import "os"
+import (
+	"os"
+	"strings"
+)
 
-func EnvGetter() ( []byte, error ){
+func EnvGetter() ( map[string]string, error ){
 
 	workingDir, _ := os.Getwd()
-
 	filePath:= workingDir + "/.env"
-	return os.ReadFile(filePath)
+	data , err:= os.ReadFile(filePath)
 
+	s:= string(data)
+	lines := strings.Split(s, "\n") // Split into lines
 
+	myMap := make(map[string]string) // Initializes an empty map
+	for _,line := range lines {
+		if line == "" {
+			continue
+		}
+		parts := strings.SplitN(line, "=",2)
+		if len(parts) != 2 {
+			continue }
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
 
+		// remove optional surrounding quotes
+		if len(value) > 1 {
+			first := value[0]
+			last := value[len(value)-1]
+			if (first == '"' && last == '"') || (first == '\'' && last == '\'') {
+				value = value[1 : len(value)-1]
+			}
+		}
+
+		myMap[key] = value
+
+	}
+	return myMap, err
 
 }
