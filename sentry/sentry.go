70456427c9681d22b7828d0a4a8b1074636c499f
CapSap <cshotam@gmail.com>
2025-11-07 16:26:18 +1100

    formatting and slightly better error message for sentry setup

--- sentry/sentry.go
+++ sentry/sentry.go
@@ -11,31 +11,30 @@ import (
 
 func Setup() {
 
-  env, err := utils.GetEnv()
+	env, err := utils.GetEnv()
 	if err != nil {
 		log.Fatalf("failed to get env: %w", err)
 	}
 
-  err = sentry.Init(sentry.ClientOptions{
-    Dsn: env["SENTRY_DSN"],
-    Debug: true,
-  })
-  if err != nil {
-    log.Fatalf("sentry.Init: %s", err)
-  }
-  // Flush buffered events before the program terminates.
-  defer sentry.Flush(2 * time.Second)
+	err = sentry.Init(sentry.ClientOptions{
+		Dsn:   env["SENTRY_DSN"],
+		Debug: true,
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	// Flush buffered events before the program terminates.
+	defer sentry.Flush(2 * time.Second)
 
-  sentry.CaptureMessage("Server started and connected to Sentry a-okay")
+	sentry.CaptureMessage("Server starting up and connected to Sentry a-okay")
 }
 
+func Notify(err error, context string) {
+	if err == nil {
+		return
+	}
 
-func Notify (err error, context string) {
-  if err == nil {
-    return 
-  }
-
-  // Add context info to the message if provided
+	// Add context info to the message if provided
 	message := fmt.Sprintf("Error: %v", err)
 	if context != "" {
 		message = fmt.Sprintf("%s | Context: %s", message, context)
