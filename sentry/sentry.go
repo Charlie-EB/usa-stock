43ddfd460057d026fc825d0b8a1fb16c7aa38de7
Charlie Morris <cshotam@gmail.com>
2025-11-07 11:50:19 +1100

    add a better message during startup
    and add a notify func to send messages to sentry

--- sentry/sentry.go
+++ sentry/sentry.go
@@ -1,6 +1,7 @@
 package sentry
 
 import (
+	"fmt"
 	"log"
 	"m/utils"
 	"time"
@@ -12,15 +13,34 @@ func Setup() {
 
   env, err := utils.GetEnv()
 	if err != nil {
-		log.Fatalf("failed to get env: %v", err)
+		log.Fatalf("failed to get env: %w", err)
 	}
 
-  err = sentry.Init(sentry.ClientOptions{Dsn: env["SENTRY_DSN"]})
+  err = sentry.Init(sentry.ClientOptions{
+    Dsn: env["SENTRY_DSN"],
+    Debug: true,
+  })
   if err != nil {
     log.Fatalf("sentry.Init: %s", err)
   }
   // Flush buffered events before the program terminates.
   defer sentry.Flush(2 * time.Second)
 
-  sentry.CaptureMessage("It works!")
+  sentry.CaptureMessage("Server started and connected to Sentry a-okay")
+}
+
+
+func Notify (err error, context string) {
+  if err == nil {
+    return 
+  }
+
+  // Add context info to the message if provided
+	message := fmt.Sprintf("Error: %v", err)
+	if context != "" {
+		message = fmt.Sprintf("%s | Context: %s", message, context)
+	}
+
+	sentry.CaptureMessage(message)
+	sentry.Flush(2 * time.Second)
 }
