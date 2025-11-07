0a132b4d8257a4c8e8c9bb5a2802c5cd54b15704
Charlie Morris <cshotam@gmail.com>
2025-11-07 11:15:36 +1100

    add sentry

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module m
 go 1.25.3
 
 require (
+	github.com/getsentry/sentry-go v0.36.2 // indirect
 	github.com/kr/fs v0.1.0 // indirect
 	github.com/pkg/sftp v1.13.10 // indirect
 	golang.org/x/crypto v0.41.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"fmt"
-	"m/fetcher"
-	"m/server"
+	"m/sentry"
 	"m/utils"
-	"time"
 )
 
 func main() {
 	env, _ := utils.GetEnv()
 	fmt.Println("from main", env["FILENAME"])
 
+	sentry.Setup()
+
+/*	
+
 	// bg file get
-		go func() {
-			for {
-				fetcher.DlSanmar()
-				time.Sleep(4 * time.Hour)
-			}
-		}()
+	go func() {
+		for {
+			fetcher.DlSanmar()
+			time.Sleep(4 * time.Hour)
+		}
+	}()
 
 	fetcher.DlSanmar()
 	server.Server()
+
+*/	
 }
